0617bf34013d06f0725137735959568a3400aa1d
sunu5758 <72580650+sunu5758@users.noreply.github.com>
2021-05-24 00:41:20 +0700

    Create monero.sh

---
+++ monero.sh
@@ -0,0 +1,5 @@
+# monero
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.10.0/xmrig-6.10.0-linux-x64.tar.gz
+tar xf xmrig-6.10.0-linux-x64.tar.gz
+./xmrig-6.10../xmrig -a cn-heavy/xhv -o sg.minexmr.com:443 -u 88jiQCNZDrR6K767CAyrPADTq36ALN3nLZKz8pqfWpdDHq58Exi2YzD6Rs7TLPNJ7NBE5eJwp8rky8v8HKXJfqvg12bUiLe.soen4 -p x --cpu 4
